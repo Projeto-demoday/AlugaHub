33fd3c607ab271ab986023539f1c8c78570043f2
Kaykjs <kaykjunior242@gmail.com>
2025-11-07 04:13:27 -0300

    Subindo definições inicais

---
+++ aluga-hub/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Sobre Nós</h1>
+      <p>Conteúdo sobre o site.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ aluga-hub/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Bem-vindo ao Meu Site!</h1>
+      <p>Esta é a página inicial. Adicione conteúdo aqui.</p>
+    </div>
+  );
+};
+
+export default Home;
