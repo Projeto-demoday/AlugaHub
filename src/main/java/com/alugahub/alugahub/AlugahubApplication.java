d9719cc49ffcc128e8a5e981da3da8a19743fb12
Victor Hugo dos santos Almeida <112269315+Victor-programmer@users.noreply.github.com>
2025-11-18 21:30:53 -0300

    k

---
+++ src/main/java/com/alugahub/alugahub/AlugahubApplication.java
@@ -0,0 +1,13 @@
+package com.alugahub.alugahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugahubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alugahub/alugahub/AlugahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugahub.alugahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
